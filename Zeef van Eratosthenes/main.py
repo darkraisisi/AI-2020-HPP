d119f4a3f8445d79b9df3f87155fca1b7b4b4b9d
David Demmers <daviddemmers@gmail.com>
2021-03-17 20:52:03 +0100

    seperated markers in chunks

--- Zeef van Eratosthenes/main.py
+++ Zeef van Eratosthenes/main.py
@@ -1,5 +1,7 @@
 from mpi4py import MPI
 import numpy as np
+import math
+import time
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
@@ -15,22 +17,35 @@ elif rank == 1:
 else:
     print(rank)
 """
+print(comm.Get_size())
+print(comm.Get_rank())
 
-N = 100
-k = 2
+def sieve(N:int):
+    chunkSize = math.ceil(N/comm.Get_size())
+    start = comm.Get_rank() * chunkSize
+    stop = start + chunkSize
 
-markers = np.ones(N+1, dtype=bool)
-markers[0] = False
-markers[1] = False
+    print(f'ID:{comm.Get_rank()}, Start:{start},Stop:{stop}')
 
-while k**2 <= N:
-    if markers[k]:
-        print('K:',k)
-        for i in range(k*2, N+1, k):
-            markers[i] = False
+    k = start + 2
 
-        print(markers,len(markers))
-        print(np.where(markers == True))
-    k+=1
+    markers = np.ones(N+1, dtype=bool)
+    markers[0] = False
+    markers[1] = False
+
+    while k**2 <= N and k < stop:
+        if markers[k]:
+            print('K:',k)
+            for i in range(k*2, N+1, k):
+                markers[i] = False
+                # Or add local counter.
+
+        k+=1
+    # Or dont count when one process is finished.
+    return len(np.where(markers[start:stop] == True)[0])
     
 # markers of prime numbers goes from 0 to n!
+startTime = time.time()
+# n = sieve(1000000000)
+n = sieve(1000)
+print(f'N found:{n}, took {time.time() - startTime} seconds.')
