8b635c94745815adf47083654356c0f2e46e8d31
David Demmers <daviddemmers@gmail.com>
2021-03-19 13:53:27 +0100

    complete mpi for sieve

--- Zeef van Eratosthenes/main.py
+++ Zeef van Eratosthenes/main.py
@@ -1,4 +1,5 @@
 from mpi4py import MPI
+import multiprocessing as mp
 import numpy as np
 import math
 import time
@@ -6,46 +7,54 @@ import time
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
-"""
-if rank == 0:
-    data = {'a': 7, 'b': 3.14}
-    print(rank,data)
-    comm.send(data, dest=1, tag=11)
-elif rank == 1:
-    data = comm.recv(source=0, tag=11)
-    print(rank,data)
-else:
-    print(rank)
-"""
-print(comm.Get_size())
-print(comm.Get_rank())
+# print(comm.Get_size())
+# print(comm.Get_rank())
 
 def sieve(N:int):
-    chunkSize = math.ceil(N/comm.Get_size())
+    startTime = time.time()
+    chunkSize = N // comm.Get_size()
     start = comm.Get_rank() * chunkSize
     stop = start + chunkSize
+    totalSum = 0
 
-    print(f'ID:{comm.Get_rank()}, Start:{start},Stop:{stop}')
+    k = 2
 
-    k = start + 2
+    if rank == 0:
+        markers = np.ones(chunkSize+1, dtype=bool)
+        markers[0] = False
+        markers[1] = False
 
-    markers = np.ones(N+1, dtype=bool)
-    markers[0] = False
-    markers[1] = False
+        while k**2 <= stop:
+            if markers[k]:
+                for i in range(k*2, stop+1, k):
+                    markers[i] = False
+            k+=1
+    else:
+        markers = np.ones(chunkSize, dtype=bool)
 
-    while k**2 <= N and k < stop:
-        if markers[k]:
-            print('K:',k)
-            for i in range(k*2, N+1, k):
-                markers[i] = False
-                # Or add local counter.
+        while k**2 <= stop:
+            if k < start or markers[k]:
+                for i in range(chunkSize):
+                    # print((start + k + i) % k, start, k, i)
+                    if (start + k + i) % k == 0:
+                        markers[i] = False
 
-        k+=1
-    # Or dont count when one process is finished.
-    return len(np.where(markers[start:stop] == True)[0])
-    
+            k+=1
+        
+
+    print(f'ID:{comm.Get_rank()}, len:{len(markers)}, Start:{start}, Stop:{stop}')
+    # for i in range(len(markers)):
+    #     if markers[i]:
+    #         print(i + (chunkSize * rank))
+
+    n = len(np.where(markers == True)[0])
+    n = comm.reduce(n, MPI.SUM, 0)
+    if rank == 0:
+        print(f'N found:{n}, took {time.time() - startTime} seconds.')
+
+
+
+
 # markers of prime numbers goes from 0 to n!
-startTime = time.time()
-# n = sieve(1000000000)
-n = sieve(1000)
-print(f'N found:{n}, took {time.time() - startTime} seconds.')
+# n = sieve(1000000)
+sieve(1000)
